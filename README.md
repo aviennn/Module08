a63f24afe6e30055ca75f227e9aec6b1d4b69fb9
aviennn <avienflairebatul@gmail.com>
2024-11-27 13:30:15 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Module08
+# Module08
---
+++ View/UserPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace Module08.View;
+using Module08.ViewModel;
+public partial class UserPage : ContentPage
+{
+	public UserPage()
+	{
+		InitializeComponent();
+		BindingContext = new UserViewModel();
+	}
+}
